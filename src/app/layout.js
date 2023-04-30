b9f39501c827efdaca777e95e9d5a37d47369cc9
Ege Aydemir <egeaydemirr97@gmail.com>
2023-04-30 18:45:10 +0300

    Head and meta tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import styles from "src/app/styles.module.css";
 import "../styles/global.css";
 import { Inter } from "@next/font/google";
+import Head from "./head";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
+      <Head />
       <body className={styles.container}>
         <header className={inter.className}>
           <p>Header</p>
